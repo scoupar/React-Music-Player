660fb53a7c93651905b71ec7ec9e758afd8727ad
Scott Coupar <scott.coupar@googlemail.com>
2021-06-22 13:41:23 +0100

    refactored

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,8 +1,29 @@
 import React from "react";
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({
+  song,
+  setCurrentSong,
+  songs,
+  id,
+  audioRef,
+  isPlaying,
+}) => {
+  const songSelectHandler = () => {
+    setCurrentSong(song);
+    //check if the song is playing
+    if (isPlaying) {
+      const playPromise = audioRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.then((audio) => {
+          audioRef.current.play();
+        });
+      }
+    }
+    audioRef.current.play();
+  };
+
   return (
-    <div className="library-song">
+    <div onClick={songSelectHandler} className="library-song">
       <img alt={song.name} src={song.cover} />
       <div className="song-description">
         <h3>{song.name}</h3>
